016cf8163a780d8b079d07bc0bd2bd48d091b763
unknown <alumno@LAB02PC12.labs.extension>
2016-08-23 12:20:15 -0300

    primer commit

---
+++ ejercicio1/Auto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    public class Auto
+    {
+        private eFabricante fabricante;
+        public Rueda ruedaDI;
+        public Rueda ruedaDD;
+        public Rueda ruedaTI;
+        public Rueda ruedaTD;
+        private int kilometrosRecorridos;
+        public static int contadorDeObjetos;
+        private static Random random;
+        
+
+        public static bool CompararAuto(Auto auto1, Auto auto2)
+        {
+            if (auto1.fabricante == auto2.fabricante)
+                return true;
+            return false;
+        }
+
+        public void MostrarAuto()
+        {
+            Console.WriteLine("El fabricante es: " + this.fabricante);
+        }
+
+        public void VolverACero()
+        {
+            this.kilometrosRecorridos = 0;
+        }
+
+        public void AgregarKilometros(int kilometros)
+        {
+            this.kilometrosRecorridos += kilometros;
+        }
+
+        public Auto()
+        {
+
+            this.fabricante = (eFabricante)Auto.random.Next(0, 3);
+            this.ruedaDD = new Rueda();
+            this.ruedaDI = new Rueda();
+            this.ruedaTD = new Rueda();
+            this.ruedaTI = new Rueda();
+            Auto.contadorDeObjetos++;
+        }
+
+        static Auto()
+        {
+            Auto.contadorDeObjetos = 0;
+            Auto.random = new Random();
+        }
+    }
+}
---
+++ ejercicio1/Rueda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    public class Rueda
+    {
+        public string marca;
+        public float tamanio;
+
+        public Rueda()
+        {
+            this.marca = "Sin Marca";
+        }
+
+        public void MostrarRueda()
+        {
+            Console.WriteLine("El tamaño de rueda es: " + this.tamanio);
+        }
+    }
+}
